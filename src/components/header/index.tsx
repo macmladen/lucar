bf550958e65a152ff8cd85baee53e65e928cfe85
Mladen Djuric <mladen@bluefish.rs>
2023-08-28 14:52:11 +0200

    Added top section with basic business information.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,11 @@
 import Logo from '@/components/header/logo'
 import NavMenu from '@/components/header/nav'
+import TopSection from '@/components/header/top-section'
 
 export default function Header() {
   return (
     <header className='dark:prose-invert'>
+      <TopSection />
       <div className='container'>
         <Logo />
         <NavMenu />
---
+++ src/components/header/top-section.tsx
@@ -0,0 +1,20 @@
+import { Clock, Mail, MapPin, Phone } from 'lucide-react'
+import Link from 'next/link'
+
+export default function TopSection() {
+  return (
+    <section className='bg-lucar-blue p-4 text-sm text-white'>
+      <p className='container'>
+        <Clock />
+        Ponedeljak – petak 09-19, Subota 09-15
+        <Phone />
+        <Link href='tel:+381642446031'>+381642446031</Link>,{' '}
+        <Link href='tel:+381638085113'>+381638085113</Link>
+        <Mail />
+        <Link href='office@lucar.rs'>office@lucar.rs</Link>
+        <MapPin />
+        Povrtarska 30, Novi Sad
+      </p>
+    </section>
+  )
+}
