40e98924eacdcca17303b894c7d4f381d6fe59d5
Mladen Djuric <mladen@bluefish.rs>
2023-08-28 14:44:45 +0200

    Removed typography demo content.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,7 @@
-import Typo from '@/components/typo'
-
 export default function About() {
   return (
     <article>
       <h1>O nama</h1>
-      <Typo></Typo>
     </article>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Joke from '@/components/typo/joke'
 import Carousel from '@/components/carousel'
 
 export default function Home() {
   return (
     <>
       <Carousel />
-      <Joke />
     </>
   )
 }
