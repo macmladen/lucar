ae4dd7e1eb542c4b053f144add395920f68e2601
Mladen Djuric <mladen@bluefish.rs>
2023-09-09 19:09:11 +0200

    First take on TNG and CNG pages, raw.

--- src/app/cng/page.tsx
+++ src/app/cng/page.tsx
@@ -1,8 +1,98 @@
 export default function Cng() {
   return (
     <section id='cng'>
-      <article className='container prose dark:prose-invert'>
-        <h1>Cng</h1>
+      <header className="bg-[url('/img/cng/cng.jpg')] bg-cover text-white">
+        <div className='container flex h-72 items-center'>
+          <h1 className='m-auto bg-lucar-gray/[0.9] px-20 py-4 text-center'>
+            <span className='text-4xl font-black'>CNG</span>
+            <br />
+            <br />
+            <span className='text-2xl'>(compressed natural gas)</span>
+          </h1>
+        </div>
+      </header>
+      <article className='container prose pt-8 dark:prose-invert'>
+        <h2>Šta je metan (cng - compressed natural gas) </h2>
+        <p>
+          Metan je po ekološkim standardima prirodni gas čije su reserve mnogo
+          veće u odnosu na rezerve nafte, a eksploatacija znatno jeftinija. Zbog
+          svojih odlika sagorevanja potrošnja je značajno manja u odnosu na
+          ostala goriva. U Srbiji je sve veće interesovanje za ugradnju metana u
+          automobile i kamione. Jedini problem je što postoji samo par pumpi
+          koje ga nude.
+        </p>
+        <h2>Ugradnja sistema </h2>
+        <p>
+          U poređenju sa autogasom koji je u tečnom stanju i predstavlja smešu
+          butana i propana, metan je u gasovitom stanju. Boce čine najveću i
+          skoro jedinu razliku u odnosu na ugradnju plina (TNG). Za sada u
+          Srbiji ima samo nekoliko profesionalnih i ovlašćenih servisa koji vrše
+          ugradnju metana u automobile. Cene su različite i zavise od više
+          faktora: model vozila, zapremina i snaga motora, broj i raspored
+          cilindara.
+        </p>
+        <p>
+          „Lucar“ poseduje svu neophodnu opremu za konverziju benzinskih i dizel
+          motora na metan (CNG), kao i tim obučenih majstora sa teorijskim
+          znanjem i višegodišnjim iskustvom u ovoj oblasti. Sve ugrađene
+          komponente poseduju homologacije proizvođača što je preduslov za
+          dobijanje atesta koji izdaju nadležne službe.
+        </p>
+        <h2>Proizvođači</h2>
+        <p>
+          Pravilnu ugradnju sistema za metan obavlja servis i serviseri obučeni
+          od strane proizvođača. Tako možete biti sigurni da serviser sarađuje
+          direktno sa proizvođačem i ima podršku u slučaju fabričke greške.
+        </p>
+        <p>
+          „Lucar“ poseduje sertifikate za ugradnju i održavanje autogas sistema
+          od strane firmi kao što su:
+        </p>
+        <h2>Atest i reatest</h2>
+        <p>
+          Kod nas možete uraditi atest ili reatest bez obzira da li je ugradnja
+          metana urađena kod nas ili u nekom drugom servisu.
+        </p>
+        <p>
+          Sva motorna vozila na kojima su instalirani sistemi moraju biti
+          usklađena sa propisanim tehničkim uslovima - pre puštanja navedenih
+          vozila u saobraćaj mora se utvditi da li vozila ispunjavaju propisane
+          uslove u skladu sa Zakonom o bezbednosti saobraćaja na putevima.
+        </p>
+        <p>
+          Da biste dobili atest i reatest vozila, ključno je da prepravka vozila
+          (ili ugradnja) bude urađena u skladu sa postojećim propisima,
+          uključujući prateću dokumentaciju.
+        </p>
+        <h2>Kako uraditi atest/reatest za plin ili metan?</h2>
+        <p>
+          „Lucar“ će vam obezbediti svu potrebnu dokumentaciju za vozila za koja
+          je ugradnja metana urađena u našem servisu. Vaše je samo da obezbedite
+          saobraćajnu dozvolu i uplatite uplatnice.{' '}
+        </p>
+        <p>
+          Za atestiranje vozila u koja ugradnja nije izvršena u našem servisu,
+          potrebno je da imate svu neophodnu dokumentaciju.{' '}
+        </p>
+        <p>
+          Za reatestiranje takođe je potrebno da donesete propratnu
+          dokumentaciju na uvid.
+        </p>
+        <h2>Servisiranje i redovno održavanje </h2>
+        <p>
+          Kada je sistem pravilno montiran i podešen, on zahteva samo redovno
+          održavanje, i to na svakih 10.000 km. U to spada zamena gasnog filtera
+          na svakom servisu. Čišćenje isparivača i zamena membrana su na 60.000
+          km. Kontrole instalacije i uređaja se obavljaju jednom godišnje.
+          Potrebno je izvršiti štelovanje nakon svake zamene filtera za vazduh
+          jer se tom prilikom menja odnos vazduha i gasa, što dovodi do toga da
+          automobil ima manju snagu.
+        </p>
+        <p>
+          Boce ne zahteva nikakvo održavanje. Kutiju multiventila treba čuvati
+          od direktnih udara težih predmeta a sam multiventil treba da je uvek
+          zaštićen.
+        </p>
       </article>
     </section>
   )
